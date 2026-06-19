8c3169c8b365f09c2f011a9711c66e5696161f5d
roliBoros <roli.boros@gmail.com>
2026-06-19 21:05:52 +0100

    feat: validate roster at startup

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './data/registry';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
